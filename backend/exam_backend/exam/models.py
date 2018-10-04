25f5d0cf6c434d009f37f7a3a4bfd8afa8c2f9b8
Alveona <pomavau@yandex.ru>
2018-10-04 21:21:23 +0300

    models updated

--- backend/exam_backend/exam/models.py
+++ backend/exam_backend/exam/models.py
@@ -2,14 +2,16 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
-    activity = models.CharField(max_length=255, null = True) # job or university
+    activity = models.CharField(max_length=255, null=True)  # job or university
     image = models.ImageField(upload_to='users/', null=True)
     phone = models.CharField(max_length=16, null=True)
     group = models.CharField(max_length=255, null=True)
-    username = models.CharField(max_length=255, default='')
-    password = models.CharField(max_length=255, default='')
+    username = models.CharField(max_length=255, default='')  # is meant to be empty
+    password = models.CharField(max_length=255, default='')  # is meant to be empty
+
 
 class Question(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
@@ -19,8 +21,10 @@ class Question(models.Model):
     image = models.ImageField(upload_to='questions/', null=True, verbose_name='Изображение')
     difficulty = models.IntegerField(null=True, verbose_name='Сложность')
     comment = models.CharField(max_length=255, blank=True)
-    answers_number = models.IntegerField(null = True)
-    audio = models.FileField(upload_to='questions_audio/', null = True)
+    answers_number = models.IntegerField(null=True)
+    audio = models.FileField(upload_to='questions_audio/', null=True)
+    attempts_number = models.IntegerField(null=True)
+    timer = models.IntegerField(null=True)  # in seconds
 
     def __str__(self):
         return '%s' % (self.title)
@@ -32,7 +36,11 @@ class Course(models.Model):
     image = models.ImageField(upload_to='courses/', blank=True, null=True, verbose_name='Изображение')
     questions = models.ManyToManyField(Question, blank=True)
     questions_number = models.IntegerField(null=True)
-    user = models.ManyToManyField(settings.AUTH_USER_MODEL, through='UserCourseRelation') # нестандартная таблица отношений
+    user = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                  through='UserCourseRelation')  # нестандартная таблица отношений
+    perfect_mark = models.IntegerField(null=True)  # percentage
+    good_mark = models.IntegerField(null=True)
+    satisfactory_mark = models.IntegerField(null=True)
 
     def __str__(self):
         return '%s' % (self.name)
@@ -44,9 +52,9 @@ class Answer(models.Model):
     correct = models.BooleanField(default=False)
     weight = models.IntegerField(blank=True, null=True)
     image = models.ImageField(upload_to='answers/', null=True, verbose_name='Изображение')
-    audio = models.FileField(upload_to='answers_audio/', null = True)
-    hint = models.CharField(max_length=255, null = True)
-    priority = models.IntegerField(null = True)
+    audio = models.FileField(upload_to='answers_audio/', null=True)
+    hint = models.CharField(max_length=255, null=True)
+    priority = models.IntegerField(null=True)
 
     def __str__(self):
         return '%s' % (self.text)
@@ -55,22 +63,23 @@ class Answer(models.Model):
 class CourseSession(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
-    attempt_number = models.IntegerField(null=True)  # attempts of current course
+    attempts_number = models.IntegerField(null=True)  # attempts of current course
     finished = models.BooleanField(default=False)  # 1 is for finished
 
 
 class SessionQuestion(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE, null=True)
     session = models.ForeignKey(CourseSession, on_delete=models.CASCADE, null=True)
-    orderNumber = models.IntegerField(null=True)
+    order_number = models.IntegerField(null=True)
     result = models.IntegerField(null=True)
-    attempt_number = models.IntegerField(null=True)  # attempts of current answer
+    attempts_number = models.IntegerField(null=True)  # attempts of current answer
     finished = models.BooleanField(default=False)  # 1 is for finished
 
 
 class SessionAnswer(models.Model):  # erased after question finished (i.e. written smth in SessionQuestion.Result
     answer = models.ForeignKey(Answer, on_delete=models.CASCADE, null=True)
     sessionQuestion = models.ForeignKey(SessionQuestion, on_delete=models.CASCADE, null=True)
+    blocked = models.BooleanField(default=False)
 
 
 # док по intermediate models:
